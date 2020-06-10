d94b7169327bd3e0017fd8e1409c121f73909346
Ellis Thompson <ellisthompson.1999@outlook.com>
2020-06-10 22:53:46 +0100

    added announce feature

---
+++ commands/announce.js
@@ -0,0 +1,46 @@
+require("discord.js");
+all_channels = [
+  // "general",
+  // "games-and-shit",
+  // "memes",
+  "staff-general",
+  // "housing",
+];
+
+module.exports = {
+  name: "announce",
+  description: "Bot Details",
+  execute(msg, args) {
+    console.log(args);
+    const channels = guild.channels;
+    if (args[1].toUpperCase() == "ALL") {
+      channels.forEach((channel) => {
+        if (all_channels.included(channel)) {
+          send_message(channel, args[2], msg);
+        }
+      });
+    } else {
+      if (channels.included(args[1])) {
+        send_message(args[1], args[2], msg);
+      }
+    }
+  },
+};
+
+function send_message(channel, message, msg) {
+  channel.send("@Owner", {
+    embed: {
+      color: "#a86832",
+      author: {
+        name: msg.member.user.tag,
+      },
+      title: "Announcement!",
+      description: message,
+      fields: [],
+      timestamp: new Date(),
+      footer: {
+        text: "- Swanbot",
+      },
+    },
+  });
+}
--- commands/index.js
+++ commands/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   Ping: require("./ping"),
-  //Exams: require("./exams"),
   Enroll: require("./enrol"),
   Version: require("./info"),
   Modules: require("./modules"),
   Drop: require("./drop"),
   Help: require("./help"),
   Xkcd: require("./xkcd"),
+  announce: require("./announce"),
 };
