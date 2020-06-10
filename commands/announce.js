f855879c01ad2a69969d86275a2705e52bc9dd5c
Ellis Thompson <ellisthompson.1999@outlook.com>
2020-06-10 23:02:37 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -14,15 +14,15 @@ module.exports = {
     console.log("ARGS==========================\n" + args);
     const channels = msg.guild.channels;
     console.log("CHANNELS==========================\n" + channels);
-    if (args[1].toUpperCase() == "ALL") {
+    if (args[0].toUpperCase() == "ALL") {
       channels.forEach((channel) => {
         if (all_channels.included(channel)) {
-          send_message(channel, args[2], msg);
+          send_message(channel, args[1], msg);
         }
       });
     } else {
-      if (channels.included(args[1])) {
-        send_message(args[1], args[2], msg);
+      if (channels.included(args[0])) {
+        send_message(args[0], args[1], msg);
       }
     }
   },
