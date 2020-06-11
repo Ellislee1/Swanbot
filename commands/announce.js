082770c792f7fed347a19d88e51d478dec89ed49
Ellis Thompson <ellisthompson.1999@outlook.com>
2020-06-11 13:45:53 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -16,53 +16,11 @@ module.exports = {
     console.log("ARGS==========================\n" + args);
     const channels = msg.guild.channels;
     console.log("CHANNELS==========================\n" + channels);
-    if (args[0].toUpperCase() == "ALL") {
-      all_channels.forEach((chan) => {
-        result = test_channel(channels, chan);
-        console.log(result);
-        if (result != false) {
-          console.log("here");
-          send_message(result, args[1], msg);
-        }
-      });
-    } else {
-      chans = args[1].split(",");
-      chans.forEach((chan) => {
-        result = test_channel(channels, chan);
-        if (result != false) {
-          send_message(result, args[1], msg);
-        }
-      });
-    }
+    channels.forEach((channel) => {
+      channel_name = channel.name;
+      if (all_channels.includes(channel_name)) {
+        channel.send("@everyone " + args[1]);
+      }
+    });
   },
 };
-
-function test_channel(channels, chan) {
-  console.log(chan);
-  channels.forEach((channel) => {
-    match = channel.name.toString() == chan;
-    if (match) {
-      return channel;
-    }
-  });
-  return false;
-}
-
-function send_message(channel, message, msg) {
-  console.log(channel);
-  bot.channels.get(channel.id).send("@everyone", {
-    embed: {
-      color: 3447003,
-      author: {
-        name: msg.member.user.tag,
-      },
-      title: "Announcement!",
-      description: message,
-      fields: [],
-      timestamp: new Date(),
-      footer: {
-        text: "- Swanbot",
-      },
-    },
-  });
-}
